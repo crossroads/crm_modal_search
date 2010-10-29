196507fe3b26294faf14a75342c0a55ae79e3dbf
Nathan B <nathan.f77@gmail.com>
2010-10-29 16:05:08 +0800

    Removed auto-complete wording

--- lib/crm_modal_auto_complete/controllers.rb
+++ lib/crm_modal_auto_complete/controllers.rb
@@ -2,7 +2,7 @@
   controller.class_eval do
     # GET /:controller/modal_search                                      AJAX
     #----------------------------------------------------------------------------
-    # Loads an rjs autocomplete template overlay
+    # Loads an rjs search template overlay
     def modal_search
       respond_to do |format|
         format.js { render 'common/modal_search', :layout => false }
